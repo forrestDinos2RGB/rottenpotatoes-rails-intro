d66d8f0f72b94a134cd5de2c77a8f506ee386dc2
Wen Chien Yen <cyen7115@berkeley.edu>
2017-07-15 00:11:48 +0000

    still not working but I believe I am close

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,12 @@ class MoviesController < ApplicationController
   end
 
   def index
+    80.times{print "*"}
+    puts "new http request begins. Examine params: #{params}"
+    80.times{print "*"}
+    #what movie are we going to show
+    @movies = Movie.all
+    
     #Set initial class of link to be none unless clicked
     @title_highlight = 'none'
     @date_highlight = 'none'
@@ -28,34 +34,44 @@ class MoviesController < ApplicationController
     #default logic for first time visiting website
     if params[:ratings] == nil && session[:ratings] == nil
       params[:ratings] = {"G"=>"1", "R"=>"1", "PG-13" => 1, "PG" => 1}
+      params[:sorted] = 'none'
       puts "first time visiting website"
-      puts "expect all ratings: #{params[:ratings]}, and nothing to be clicked #{session[:all_keys]}"
+      puts "expect all ratings: #{params[:ratings]}, and nothing to be clicked #{session[:ratings]}"
     end
 
     #Did user check any of the checkbox in form tag?
     if params[:ratings] == nil
       @redirect_enable = true
       #update the params package in case nothing has been passed
-      params_package[:ratings] = session[:ratings]
+      @params_package[:ratings] = session[:ratings]
     else
       #update the cookie
       session[:ratings] = params[:ratings]
     end
 
     #Did user click on either title or ratings?
-    clicked = params[:sorted]
+    clicked ||= params[:sorted]
     if clicked == 'title'
       @title_highlight = 'hilite'
       session[:sorted] = 'title'
+      @movies = @movies.where(params[:ratings].keys).order(:title)
     elsif clicked == 'date'
       @date_highlight = 'hilite'
       session[:sorted] = 'date'
+      @movies = @movies.where(params[:ratings.keys]).order(:release_date)
+    elsif clicked == 'none'
+      puts "neither title nor rating has been clicked"
+      session[:sorted] = 'none'
     else
       @redirect_enable = true
-      params_package[:sorted]
+      @params_package[:sorted] = session[:sorted]
     end
-
+    
     if @redirect_enable
+      #update the params package when redirecting
+      @params_package[:sorted] = session[:sorted]
+      #update the params package when redirecting
+      @params_package[:ratings] = session[:ratings]
       redirect_to movies_path(@params_package)
     end
 
